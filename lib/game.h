a0fea172a63c387974864b279fb7f7e75d931910
Alejandro Jara <a00090040@gmail.com>
2023-04-25 02:59:20 +1000

    add game.h a file that will import all the libraries for the game

---
+++ lib/game.h
@@ -0,0 +1,20 @@
+#ifndef GAME_H
+#define GAME_H
+
+/**
+ * @file game.h
+ * @author your name (you@domain.com)
+ * @brief  This file is meant to import all the necesarry libraries for the project.
+ * @version 0.1
+ * @date 2023-04-25
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+// Include all the libraries inside the Lib folder.
+#include "player.h"
+#include "power_up.h"
+#include "button.h"
+
+#endif
