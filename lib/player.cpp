9b4003f45bba09350e38846bd2919d99fa4909a4
Alejandro Jara <a00090040@gmail.com>
2023-04-27 02:02:28 +1000

    initialize all new properties of player structure and implement full screen functionality in handle_input function

--- lib/player.cpp
+++ lib/player.cpp
@@ -26,6 +26,7 @@ bitmap ship_bitmap(ship_kind kind)
 player_data new_player()
 {
     player_data result;
+
     bitmap default_bitmap = ship_bitmap(AQUARII);
 
     // Create the sprite with 3 layers - we can turn on and off based
@@ -45,6 +46,21 @@ player_data new_player()
     sprite_set_x(result.player_sprite, (screen_width() - sprite_width(result.player_sprite))/2.0);
     sprite_set_y(result.player_sprite, (screen_height() - sprite_height(result.player_sprite))/2.0);
 
+    // initialize player's fuel
+    result.fuel_pct = 0.75;
+
+    // initialize health
+    result.health = 1;
+
+    // initialize shield
+    result.shield = 0;
+
+    // initialize ammo
+    result.ammunition = 0;
+
+    // initialize score
+    result.score = 0;
+    
     return result;
 }
 
@@ -57,9 +73,10 @@ void update_player(player_data &player_to_update)
 {
     // Apply movement based on rotation and velocity in the sprite
     update_sprite(player_to_update.player_sprite);
-
     // Test edge of screen boundaries to adjust the camera
     double left_edge = camera_x() + SCREEN_BORDER;
+    //double left_edge = center_point(player_to_update.player_sprite).x;
+    //double right_edge = left_edge;
     double right_edge = left_edge + screen_width() - 2 * SCREEN_BORDER;
     double top_edge = camera_y() + SCREEN_BORDER;
     double bottom_edge = top_edge + screen_height() - 2 * SCREEN_BORDER;
@@ -109,8 +126,26 @@ void player_switch_to_ship(player_data &player, ship_kind target)
     }
 }
 
+bool is_full_screen = false;
 void handle_input(player_data &player)
 {
+    // Allow the player to switch between full screen mode and normal screen.
+    if(key_typed(F11_KEY))
+    {
+        if(!is_full_screen)
+        {
+            move_current_window_to(0,30);
+            resize_current_window(1920, 1080);
+            is_full_screen = true;
+        }
+        else
+        {
+            move_current_window_to(368, 132);
+            resize_current_window(800,600);
+            is_full_screen = false;
+        }
+    }
+
     // Allow the player to switch ships
     if (key_typed(NUM_1_KEY))
         player_switch_to_ship(player, AQUARII);
@@ -120,7 +155,7 @@ void handle_input(player_data &player)
         player_switch_to_ship(player, PEGASI);
 
     // Handle movement - rotating left/right and moving forward/back
-    float dx = sprite_dx(player.player_sprite);
+    //float dx = sprite_dx(player.player_sprite);
     float rotation = sprite_rotation(player.player_sprite);
 
     // Allow rotation with left/right keys
@@ -130,8 +165,8 @@ void handle_input(player_data &player)
         sprite_set_rotation(player.player_sprite, rotation + PLAYER_ROTATE_SPEED);
 
     // Increase speed with up/down keys - typed to give step increases
-    if (key_typed(DOWN_KEY))
-        sprite_set_dx(player.player_sprite, dx - PLAYER_SPEED);
-    if (key_typed(UP_KEY))
-        sprite_set_dx(player.player_sprite, dx + PLAYER_SPEED);
+    if (key_down(UP_KEY))
+        sprite_set_dx(player.player_sprite, 2*PLAYER_SPEED);
+    else if(key_up(UP_KEY))
+        sprite_set_dx(player.player_sprite, 0);
 }
